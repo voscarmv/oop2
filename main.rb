1fce7694dc2e858bcd2d52bd9f5b7fef3430c6f7
pwambui93@gmail.com <pwambui93@gmail.com>
2021-09-14 10:52:05 +0300

    Create the main entry function

--- main.rb
+++ main.rb
@@ -1,5 +1,10 @@
 class App
-
+    def initialize
+        @books = []
+        @people = []
+        @rentals = []
+    end
+    
     def all_books
         # Lists all books
     end
@@ -26,7 +31,41 @@ class App
 end
 
 def main
+    app = App.new()
+    selection = nil
+
+    unless selection === "7"
+        puts "Please choose an option by enterin a number:"
+        puts "1 - List all books"
+        puts "2 - List all people"
+        puts "3 - Create a person"
+        puts "4 - Create a book"
+        puts "5 - Create a rental"
+        puts "6 - List all rentals for a given person id"
+        puts "7 - Exit"
+        print "Add your selection"
+        print " "
+        selection = gets.chomp
     
+        case selection
+        when "1"
+          app.all_books()
+        when "2"
+          app.all_people()
+        when "3"
+          app.create_person()
+        when "4"
+          app.create_book()
+        when "5"
+          app.create_rental()
+        when "6"
+          app.list_rentals()
+        when "7"
+          puts "Thank you for using this app!"
+        end
+    
+        puts "\n"
+      end
 end
 
 main()
